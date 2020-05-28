14fa29f6ed8c8b0e4daeef7e68f201575f9f4a2f
Weather Spud <weatherspud@gmail.com>
2020-05-27 17:47:05 -0700

    enforce product type restriction

--- checklist.py
+++ checklist.py
@@ -6,6 +6,7 @@ import sys
 PRODUCT_TYPES = ['box set', 'rulebook', 'supplement', 'scenario', 'replay', 'periodical', 'gamebook']
 HEADER = ['product_type', 'japanese', 'english', 'publisher', 'year']
 
+
 def checklist(paths, fout):
     writer = csv.writer(fout)
     writer.writerow(['category'] + HEADER)
@@ -15,6 +16,10 @@ def checklist(paths, fout):
             header = None
             for row in csv.reader(f):
                 if header:
+                    if 'product_type' in header and row[header.index('product_type')]:
+                        product_type = row[header.index('product_type')]
+                        if product_type not in PRODUCT_TYPES:
+                            raise Exception('{}: not a product type {}'.format(path, product_type))
                     outrow = [row[header.index(h)] if h in header else ''
                               for h
                               in HEADER]
